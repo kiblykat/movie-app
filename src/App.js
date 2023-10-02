0ddf205f58239ae44ded3b8fd463f52aca469935
izzat <izzatfadzlon@gmail.com>
2023-10-02 12:55:44 +0800

    done: search function working. App is usable

--- src/App.js
+++ src/App.js
@@ -11,26 +11,33 @@ const API_URL = 'http://www.omdbapi.com?apikey=66b435d1'
 
 const App = () => {
   const [movies,setMovies] = useState([])
+  const [search,setSearch] = useState('')
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
 
     setMovies(data.Search)
-    console.log(data.Search)
   }
   useEffect(()=>{
-    searchMovies('sdasdas')
+    searchMovies('')
   },[])
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleUserSearch = () => {
+    searchMovies(search)
+  }
 
   
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input placeholder="Search for movies" value="Superman" />
-        <img src={searchIcon}/>
+        <input placeholder="Search for movies" value={search} onChange={handleChange} />
+        <img src={searchIcon} onClick={handleUserSearch}/>
       </div>
       <div className="container">
         {
